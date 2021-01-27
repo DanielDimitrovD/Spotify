a93b2858bfa9678bdda212c660b64ee1ea9b3f40
DanielDimitrovD <dakata619@gmail.com>
2021-01-27 11:50:33 +0200

    Add getters to ClientConnectionException

--- src/bg/sofia/uni/fmi/mjt/spotify/ClientExceptions/ClientConnectionException.java
+++ src/bg/sofia/uni/fmi/mjt/spotify/ClientExceptions/ClientConnectionException.java
@@ -1,8 +1,22 @@
 package bg.sofia.uni.fmi.mjt.spotify.ClientExceptions;
 
-import java.io.IOException;
+public class ClientConnectionException extends RuntimeException {
 
-public class ClientConnectionException extends RuntimeException{
+    private String message;
+    private Throwable cause;
+
     public ClientConnectionException(String s, Throwable cause) {
+        this.message = s;
+        this.cause = cause;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public Throwable getCause() {
+        return cause;
     }
 }
