5a5aa409383b2292552a16ee1bbbfd61daf05072
DanielDimitrovD <dakata619@gmail.com>
2021-01-27 11:33:53 +0200

    Add simple SpotifyClient

---
+++ src/bg/sofia/uni/fmi/mjt/spotify/SpotifyClient.java
@@ -0,0 +1,59 @@
+package bg.sofia.uni.fmi.mjt.spotify;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+public class SpotifyClient {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_HOST = "localhost";
+    private static ByteBuffer buffer = ByteBuffer.allocateDirect(512);
+    
+    public void startClient() {
+
+        try (SocketChannel socketChannel = SocketChannel.open()) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            socketChannel.connect(new InetSocketAddress(SERVER_HOST, SERVER_PORT));
+
+            System.out.println("connected to server");
+
+            while (true) {
+
+                //    System.out.println("Enter message");
+                String message = String.format("%s%n", scanner.nextLine());
+                //    System.out.println("Sending message <" + message + "> to the server...");
+
+                buffer.clear();
+                buffer.put(message.getBytes());
+                buffer.flip();
+                socketChannel.write(buffer);
+
+                buffer.clear();
+                socketChannel.read(buffer);
+                buffer.flip();
+
+                byte[] byteArray = new byte[buffer.remaining()];
+                buffer.get(byteArray);
+
+                String reply = new String(byteArray, "UTF-8");
+
+                System.out.print(reply);
+
+                if (reply.contains("Disconnected")) {
+                    return;
+                }
+
+            }
+        } catch (IOException e) {
+            System.out.println("There is a problem with the network communication");
+            e.printStackTrace();
+        }
+
+    }
+
+}
